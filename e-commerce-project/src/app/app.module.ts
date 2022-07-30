60dc2fa1427dff687632222fce6f294ecbef1df3
oldroge <rogeriomrjr@gmail.com>
2022-07-30 15:08:48 -0300

    feat: create a header component

--- e-commerce-project/src/app/app.module.ts
+++ e-commerce-project/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { coreModule } from './core-module/modules';
+import { NgModule, enableProdMode } from '@angular/core';
+import { coreModule } from './core-module/core.modules';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+enableProdMode();
 @NgModule({
   declarations: [
     AppComponent
