58ef4a83413f83e72639eabb02634caa97fca306
oldroge <rogeriomrjr@gmail.com>
2022-07-31 15:29:33 -0300

    build: import HTTPClientModule

--- e-commerce-project/src/app/app.module.ts
+++ e-commerce-project/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule, enableProdMode } from '@angular/core';
 import { coreModule } from './core-module/core.modules';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 enableProdMode();
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    coreModule
+    coreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
